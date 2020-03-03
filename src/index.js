e1cc9e216c8b2d05435d4bf46fbfd567c2ad0b96
Vladimir Veraks <vladimirveraksa@gmail.com>
2020-03-03 13:43:02 +0300

    feat: done typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
 exports.min = function min (array) {
-  return 0;
+
+    if(array === undefined || array.length === 0 || array === 0){
+        return 0;
+    }
+
+  return array.sort((a, b) => {
+      return a - b;
+  })[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+
+    if(array === undefined || array.length === 0 || array === 0){
+        return 0;
+    }
+        return array.sort((a, b) => {
+            return a - b;
+        })[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let result = 0;
+    if(array === undefined || array.length === 0 || array === 0){
+        return result;
+    }
+
+    result = array.reduce((sum, item) => {
+      return  sum+item;
+    });
+
+  return result / array.length;
 }
+
+
